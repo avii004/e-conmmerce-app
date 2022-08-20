50108b7d36550930641b3ef95c6f17d78a4fb084
Gracie Navarro <100546819+avii004@users.noreply.github.com>
2022-08-20 16:27:49 -0500

    Add files via upload

---
+++ routes/api/category-routes.js
@@ -0,0 +1,64 @@
+const router = require('express').Router();
+const { Category, Product } = require('../../models');
+
+
+router.get('/', (req, res) => {
+
+  Category.findAll({
+    include: [{ model: Product }],
+  }).then((categories) => res.status(200).json(categories))
+    .catch((err) => {
+      console.log(err);
+      res.status(400).json(err);
+    });
+});
+
+router.get('/:id', (req, res) => {
+ 
+  Category.findByPk(req.params.id, {
+    include: [{ model: Product }],
+  }).then((category) => res.status(200).json(category))
+    .catch((err) => {
+      console.log(err);
+      res.status(400).json(err);
+    });
+});
+
+router.post('/', (req, res) => {
+  
+  Category.create(req.body)
+    .then((category) => res.status(200).json(category))
+    .catch((err) => {
+      console.log(err);
+      res.status(400).json(err);
+    });
+});
+
+router.put('/:id', (req, res) => {
+
+  Category.update(req.body, {
+    where: {
+      id: req.params.id,
+    },
+  }).then((category) => res.status(200).json(category))
+    .catch((err) => {
+      console.log(err);
+      res.status(400).json(err);
+    });
+});
+
+router.delete('/:id', (req, res) => {
+
+
+  Category.destroy({
+    where: {
+      id: req.params.id,
+    },
+  }).then((category) => res.status(200).json(category))
+    .catch((err) => {
+      console.log(err);
+      res.status(400).json(err);
+    });
+});
+
+module.exports = router;
